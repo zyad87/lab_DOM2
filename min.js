b9ef93f6954e3ac5ac88037abf3709883072afc3
zyad87 <zyadt871@gmail.com>
2024-09-11 13:54:56 +0300

    first commit

---
+++ min.js
@@ -0,0 +1,18 @@
+let trin = document.getElementsByClassName('trin')[0];
+console.log(trin);
+
+colors = ['red', 'black', 'blue', 'yellow', 'green', 'purple', 'pink'];
+
+setInterval(() => {
+  let positionx = Math.random() * 50;
+  let positiony = Math.random() * 50;
+  trin.style.left = `${positionx}px`;
+  trin.style.bottom = `${positiony}px`;
+  trin.style.right = `${positionx}px`;
+  trin.style.top = `${positiony}px`;
+  trin.style.backgroundColor =
+    '#' + (((1 << 24) * Math.random()) || 0).toString(16).padStart(6, '0');
+  trin.style.width = `${Math.random() * 200}px`;
+  trin.style.height = `${Math.random() * 200}px`;
+  trin.style.opacity = Math.random();
+}, 2000);
